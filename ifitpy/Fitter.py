ba07b3533078e7aebd9161a42c897964d95bb000
Guilherme Pereira <guilherme.pereira@coimbra.lip.pt>
2025-03-15 01:42:07 +0000

    general optimizations and improvements

--- ifitpy/Fitter.py
+++ ifitpy/Fitter.py
@@ -23,8 +23,6 @@ class Utils(object):
     # Used to estimate the initial means of the gaussian fit
     @staticmethod
     def get_xx_of_n_max(xx, yy, n):
-        
-
         w=5 #moving average with a window of length
         ty=np.convolve(yy, np.ones(w), 'same') / w
 
@@ -46,16 +44,16 @@ class Utils(object):
 #Result class to generate named-values that can get used after a fit 
 #outputs: 
 #    vars: [np.float64(3346), np.float64(49), np.float64(10)], amp: 3346, mean: 49, sigma: 10
-class Result(object):
-    def __init__(self, dict, identity=""):
-        self.vars = []
-        for varname in dict:
-            setattr(self, varname, dict[varname])
-            self.vars.append(dict[varname])
-            
+class Result:
+    def __init__(self, params_dict):
+        self.vars = list(params_dict.values())
+        for varname in params_dict:
+            setattr(self, varname, params_dict[varname])
+
+        self._params = params_dict
+
     def __str__(self):
-        out= ", ".join([key + ": "+ str(self.__dict__[key]) for key in self.__dict__.keys()])
-        return out
+        return ", ".join(f"{key}: {val}" for key, val in self._params.items())
 
 
 #Prepares pre-defined functions for fitting
@@ -91,28 +89,27 @@ class Functions(object):
 
 class Fitter(object):
 
-    def __init__(self, fittype, *args):
+    def __init__(self, fit_type, custom_func=None):
         
-        self.func = None # based function for fitting
-        self.params = None # Results after fitting
+        self._func = None # based function for fitting
+        self._params = None # Results after fitting
         self.err = None # errors of each parameter found in the fit
         self.par = None # list with paramater. Mainly used for internal evaluation
-        self.p0 = None # list with paramater. Mainly used for internal evaluation
-        self.chi2 = None # chi2 of the fit
-        self.dof = None # degrees of the fit
-        self.fittype = fittype
-        if fittype == "linear":
-            self.func = Functions.lin
-        elif fittype == "gaussian":
-            self.func = Functions.gaussian
-        elif fittype == "gaussian2d":
-            self.func = Functions.gaussian_2d
-        elif fittype == "expo":
-            self.func = Functions.expo
-        elif fittype == "poly":
-            self.func = Functions.lin
-        self.ident = ",".join(self.func.__code__.co_varnames[:self.func.__code__.co_argcount]) # to print the arguments of the fittin function (x,amp,mean,sigma in case of Gaussian)
-        self.func_out = self.func # actuall fitting function, which sometimes can be a sum of self.func
+        self._p0 = None # list with paramater. Mainly used for internal evaluation
+        self.chi2 = np.inf # chi2 of the fit
+        self.dof = np.inf # degrees of the fit
+        self._fittype = fit_type
+
+        funcs_opts = {"linear": Functions.lin, "gaussian": Functions.gaussian, "gaussian2d": Functions.gaussian_2d,
+                     "gaussian2d": Functions.gaussian_2d, "expo": Functions.expo, "poly": Functions.lin}
+
+        if custom_func:
+            self._func = custom_func
+        else:
+            self._func = funcs_opts.get(self._fittype, None)
+
+        self.ident = ",".join(self._func.__code__.co_varnames[:self._func.__code__.co_argcount]) # to print the arguments of the fittin function (x,amp,mean,sigma in case of Gaussian)
+        self._func_out = self._func # actual fitting function, which sometimes can be a sum of self._func
         self.binned = False # wether to bin data or not
 
         #information for the profile histogram
@@ -128,7 +125,7 @@ class Fitter(object):
     def enableMinuit(self):
         self.minuitactive = True
 
-    # Makes histograms from raw data 
+    # Makes fit with internal histograms from raw data 
     def fitBinned(self, x, y=np.array([]), p0=None, bins=100, n=1):
         x, y = np.array(x), np.array(y)
         prof = None
@@ -146,29 +143,29 @@ class Fitter(object):
     def fit(self, x, y=None, yerr=None, p0=None,n=1):
         x, y = np.array(x), np.array(y)
 
-        if self.fittype == "gaussian":
-            self.fitGauss(x,y,yerr,p0,n)
+        if self._fittype == "gaussian":
+            self._fitGauss(x,y,yerr,p0,n)
         
-        elif self.fittype == "gaussian2d":
-            self.fitGauss2D(x,y,p0,n)
+        elif self._fittype == "gaussian2d":
+            self._fitGauss2D(x,y,p0,n)
 
-        elif self.fittype == "linear":
-            self.fitLin(x,y,yerr,p0)
+        elif self._fittype == "linear":
+            self._fitLin(x,y,yerr,p0)
 
-        elif self.fittype == "expo":
-            self.fitExpo(x,y,yerr,p0) 
+        elif self._fittype == "expo":
+            self._fitExpo(x,y,yerr,p0) 
 
-        elif self.fittype == "poly":
-            self.fitPoly(x,y,p0) 
+        elif self._fittype == "poly":
+            self._fitPoly(x,y,p0) 
 
     #x, y and yerr are ordered
-    def fitGauss(self, x, y=None, yerr=None, p0=None,n=1):
+    def _fitGauss(self, x, y=None, yerr=None, p0=None,n=1):
 
         #prepare a function that is represented by the sum of n gaussians
         def ngaussianfit(x, *params): #amp_l, mean_l, sigma_l
             y = np.zeros_like(x)
             for i in range(0, len(params), 3):
-                y += self.func(x, params[i], params[i+1], params[i+2])
+                y += self._func(x, params[i], params[i+1], params[i+2])
             return y 
 
         # in case of using n paramters to define the number of gaussians 
@@ -181,7 +178,7 @@ class Fitter(object):
 
             # calculated means based on splines. 
             # The output doe snto exists necessarly in x
-            means = np.sort(Utils.get_xx_of_n_max(x,y,n))
+            means = np.sort(Utils.get_xx_of_n_max(x,y,n))[:n]
 
             p0 = []
             for s in range(0,n):
@@ -202,27 +199,23 @@ class Fitter(object):
                 p0 += [estimated_amp , true_mean, xl]
 
         #prepare parameters boundaries 
-        bounds_hi = []
-        bounds_lo = []
-        for i in range(0,n):
-            bounds_lo += [0,-np.inf,0]
-            bounds_hi += [np.inf,np.inf,np.inf]
-        par, cov = self.fitter(ngaussianfit, x, y, yerr, p0=p0,bounds=(bounds_lo, bounds_hi))
+        bounds_lo = [0,-np.inf,0]*n
+        bounds_hi = [np.inf,np.inf,np.inf]*n
 
-        #prepare named values
-        pars_dict = {}
-        if len(par)<=3:
-            pars_dict = {"amp":par[0],"mean":par[1], "sigma":par[2]} 
+
+        varnames = []
+
+        if len(p0)<=3:
+            varnames = ["amp","mean", "sigma"]
         else:
             for i in range(0, len(p0),3):
-                pars_dict["amp_"+str(int(i/3))] = par[i] 
-                pars_dict["mean_"+str(int(i/3))] = par[i+1] 
-                pars_dict["sigma_"+str(int(i/3))] = par[i+2] 
+                varnames.append("amp_"+str(int(i/3)))
+                varnames.append("mean_"+str(int(i/3)))
+                varnames.append("sigma_"+str(int(i/3)))
 
-        self.params = Result(pars_dict)
-        self.func_out = ngaussianfit
+        par, cov = self._fitter(ngaussianfit, x, y, yerr, p0=p0,bounds=(bounds_lo, bounds_hi), param_names=varnames)
 
-    def fitGauss2D(self, x, y, p0=None, n=1):
+    def _fitGauss2D(self, x, y, p0=None, n=1):
 
         pf2d = ht.Profile2D(binsx=400, binsy=400)
         pf2d.fill(x, y)
@@ -233,7 +226,7 @@ class Fitter(object):
             z = np.zeros_like(xy[0])
             #print(z)
             for i in range(0, len(params), 6):
-                g1 = self.func(xy, *params[i:i+6])
+                g1 = self._func(xy, *params[i:i+6])
                 z += g1
             return z
 
@@ -249,21 +242,20 @@ class Fitter(object):
                 yg = y[tcut]
                 p0 += [np.average(xg),np.average(yg),np.std(xg),np.std(yg), zh_R.max()/n, -0.001]
 
-        par, cov = self.fitter(func=ngaussian2d,x=(ah_R,bh_R),y=zh_R, yerr=sigs,p0=p0)
+        varnames = []
 
-        pars_dict = {}
         for i in range(0, len(p0),6):
-            pars_dict["x0_"+str(int(i/6))] = par[i] 
-            pars_dict["y0_"+str(int(i/6))] = par[i+1] 
-            pars_dict["sigma_x_"+str(int(i/6))] = par[i+2] 
-            pars_dict["sigma_y_"+str(int(i/6))] = par[i+3] 
-            pars_dict["amp_"+str(int(i/6))] = par[i+4] 
-            pars_dict["theta_"+str(int(i/6))] = par[i+5] 
+            varidx = int(i/6)
+            varnames.append("x0_"+str(varidx))
+            varnames.append("y0_"+str(varidx))
+            varnames.append("sigma_x_"+str(varidx))
+            varnames.append("sigma_y_"+str(varidx))
+            varnames.append("amp_"+str(varidx))
+            varnames.append("theta_"+str(varidx))
 
-        self.params = Result(pars_dict)
-        self.func_out = ngaussian2d
+        par, cov = self._fitter(ngaussian2d, x=(ah_R,bh_R),y=zh_R, yerr=sigs, p0=p0, param_names=varnames)
 
-    def fitLin(self,x,y,yerr=None,p0=None):
+    def _fitLin(self,x,y,yerr=None,p0=None):
         
         if p0 == None:
             y0, y1 = np.min(y), np.max(y)
@@ -272,12 +264,10 @@ class Fitter(object):
             b0 = y0-m0*x0
             p0 = (m0,b0)
 
-        par, cov = self.fitter(self.func,x,y,yerr,p0=p0)
+        varnames = ["m", "b"]
+        par, cov = self._fitter(self._func, x=x,y=y, yerr=yerr, p0=p0, param_names=varnames)
 
-        pars_dict = {"m":par[0],"b":par[1]} 
-        self.params = Result(pars_dict)
-
-    def fitExpo(self, x, y, yerr=None, p0=None):
+    def _fitExpo(self, x, y, yerr=None, p0=None):
 
         if p0 == None:
             t = (y>0)
@@ -286,12 +276,10 @@ class Fitter(object):
             p2,p1 = Utils.makeExpoParam(x0,y0,x1,y1)
             p0 = [p2, p1]
 
-        par, cov = self.fitter(self.func,x,y,yerr,p0=p0)
+        varnames = ["p0", "p1"]
+        par, cov = self._fitter(self._func, x=x, y=y, yerr=yerr, p0=p0, param_names=varnames)
 
-        pars_dict = {"p0":par[0],"p1":par[1]} 
-        self.params = Result(pars_dict)
-
-    def fitPoly(self, x, y,p0=None):
+    def _fitPoly(self, x, y,p0=None):
         
         def multipoly(x, *params):
             z = np.zeros_like(x)
@@ -310,28 +298,27 @@ class Fitter(object):
                 yg = y[s*step:s*step+step]
                 m = (np.max(yg)-np.min(yg))/(xg[yg==np.max(yg)][0]-xg[yg==np.min(yg)][0])
                 p0 += [m]
-        
-        par, cov = self.fitter(multipoly,x,y,p0=p0)
 
         pnames = string.ascii_lowercase
-        pars_dict = {}
+        varnames = []
         for i in range(0, len(p0)):
-            pars_dict[pnames[i]] = par[i] 
-        
-        self.params = Result(pars_dict)
-        self.func_out = multipoly
+            varnames.append(pnames[i])   
+               
+        par, cov = self._fitter(multipoly, x=x, y=y, p0=p0, param_names=varnames)
 
-    def fitter(self,func,x,y,yerr=np.array([]),p0=None, bounds=None):
+
+    def _fitter(self, func, x, y, yerr=np.array([]), p0=None, bounds=None, param_names=None):
         
-        self.p0 = p0
+        self._p0 = p0
+        self._func_out = func
         if isinstance(yerr, np.ndarray): 
             if((yerr==None).all()):
                 yerr = np.array([1e-9] * y.shape[0])
         elif yerr == None:
             yerr = np.array([1e-9] * y.shape[0])
         
-        if bounds == None or self.fittype == "gaussian2d":
-            if (self.fittype == "gaussian2d"):
+        if bounds == None or self._fittype == "gaussian2d":
+            if (self._fittype == "gaussian2d"):
                 yerr=None
 
             par, cov = curve_fit(func, x, y, sigma=yerr, p0=p0, maxfev = 10000, xtol=1e-8)
@@ -350,6 +337,10 @@ class Fitter(object):
             if cov is None:continue
             vars.append(cov[i][i]**0.5)
 
+        pars_dict = {name: par[i] for i, name in enumerate(param_names)}
+        self._params = Result(pars_dict)
+        self._func_out = func
+
         self.err = vars
         self.par = par
         self.chi2 = 0 #m.fval
@@ -358,25 +349,25 @@ class Fitter(object):
 
 
     def getParams(self):
-        return self.params
+        return self._params
 
     def getInitParams(self):
-        return self.p0
+        return self._p0
 
     def getErrors(self):
         return self.err
     
     def function(self):
-        return self.func_out
+        return self._func_out
     
-    def evaluate(self, xx,yy=None):
+    def evaluate(self, xx, yy=None):
         
         try:
             xx = np.array(xx)
-            return self.func_out(xx,*self.par)
+            return self._func_out(xx,*self.par)
         except Exception:
             xx, yy = np.array(xx),np.array(yy) 
-            return self.func_out(xx, yy,*self.par)
+            return self._func_out(xx, yy,*self.par)
         except TypeError:
             print("Invalid input for function")
 
